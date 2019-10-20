b905e78acfbce189b33f08b2accd2d52187a7e27
Diogo Ferreira <diogo@otus-solutions.com.br>
2019-10-20 00:19:08 -0300

    OTUS-605 Correções em variaveis

--- terraform/build-image/images.tf
+++ terraform/build-image/images.tf
@@ -5,25 +5,21 @@
 variable "otus-domain-api-name" {
   default = "otus-domain-api"  
 }
-variable "otus-domain-api-directory" {
-  default = "otus-domain-api"  
-}
+
 variable "otus-domain-api-source" {
-  default = "/source/otus-domain-root"  
+  default = "source"  
 }
 
 variable "otus-domain-api-mvnbuild" {
   default = "clean install"
-  
 }
-
 
 ###############################################
 ### OTUS-DOMAIN-API : Build Image Service ###
 ###############################################
 resource "null_resource" "otus-domain-api-build" {
   provisioner "local-exec" {
-    working_dir = "otus-domain-api/source/otus-domain-root"
+    working_dir = "${var.otus-domain-api-source}/otus-domain-root"
     command = "mvn ${var.otus-domain-api-mvnbuild}"
   }
 }
@@ -31,7 +27,6 @@ resource "null_resource" "otus-domain-api-build" {
 resource "null_resource" "otus-domain-api" {
   depends_on = [null_resource.otus-domain-api-build]
   provisioner "local-exec" {
-    working_dir = "otus-domain-api"
     command = "docker build -t ${var.otus-domain-api-name} ."
   }
 }
--- terraform/run-container/containers.tf
+++ terraform/run-container/containers.tf
@@ -1,26 +1,32 @@
-variable "otus-domain-api" {
-  type = "map"
-  default = {
-	"name" = "otus-domain-api"
-	"persistence-directory" = "/home/drferreira/otus-platform/docker-persistence/otus-domain-api"
-	"port_http" = 51006
-	"port_management" = 51008
-  }
+variable "otus-domain-api-persistence"{
+  default = "/otus-platform/docker-persistence/otus-domain-api"
+}
+
+variable "otus-domain-api-porthttp"{
+  default = 51006
+}
+
+variable "otus-domain-api-portmanagement"{
+  default = 51008
+}
+
+variable "otus-domain-api-version"{
+  default = "latest"
 }
 
 resource "docker_image" "otus-domain-api" {
-  name = "otus-domain-api:latest"
+  name = "otus-domain-api:${var.otus-domain-api-version}"
 }
 
 resource "docker_container" "otus-domain-api" {
   name = "otus-domain-api"
-  image = "${docker_image.otus-domain-api.latest}"
+  image = "${docker_image.otus-domain-api.name}"
   ports {
 	internal = 8080
-	external = "${var.otus-domain-api["port_http"]}"
+	external = "${var.otus-domain-api-porthttp}"
   }
   ports {
 	internal = 9990
-	external = "${var.otus-domain-api["port_management"]}"
+	external = "${var.otus-domain-api-portmanagement}"
   }
 }
